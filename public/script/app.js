747ae96bb1d19b9b8348c17d83f042d7fbbc985b
Deep3226 <deepatel099@gmail.com>
2022-10-03 22:44:46 -0400

    Add existing project files to Git

---
+++ public/script/app.js
@@ -0,0 +1,18 @@
+/**
+  * app.js
+  * Deep Rakeshkumar Patel
+  * 301239480
+  * Oct 2, 2022
+  */
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
